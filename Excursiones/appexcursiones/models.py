b242c780c071dbe6e7d893b7c1c3f5444dd6fb45
Rafael Roman <rafaelroman01@gmail.com>
2023-10-04 17:00:52 -0300

    Creacion de modelos

--- Excursiones/appexcursiones/models.py
+++ Excursiones/appexcursiones/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Viajes(models.Model):
+    nombre = models.CharField(max_length=50)
+    destino = models.CharField(max_length=50)
+    grupo = models.IntegerField()
+    email= models.EmailField(max_length=80)
+    pass 
+
+class Recreadores(models.Model):
+   nombre = models.CharField(max_length=50)
+   apellido = models.CharField(max_length=50)
+   dni = models.IntegerField()
+   edad = models.IntegerField()
+   email= models.EmailField(max_length=80)
+  
+class Clientes(models.Model):
+   nombre = models.CharField(max_length=50)
+   apellido = models.CharField(max_length=50)
+   dni = models.IntegerField()
+   edad = models.IntegerField()
+   email= models.EmailField(max_length=80)
+   
+
+class Proveedores(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    dni = models.IntegerField()
+    edad = models.IntegerField()
+    email= models.EmailField(max_length=80) 
+
+class Documentacion(models.Model):
+    nombre = models.CharField(max_length=50)
+    fechatope = models.DateField()
+    entregado = models.BooleanField()
+    email= models.EmailField(max_length=80)
+
+    
