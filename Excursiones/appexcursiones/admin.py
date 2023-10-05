f2053bf2b73309dc9c68013f9c607d0f158a484c
Rafael Roman <rafaelroman01@gmail.com>
2023-10-05 09:48:09 -0300

    Creacion panel de Administracion

--- Excursiones/appexcursiones/admin.py
+++ Excursiones/appexcursiones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from appexcursiones.models import *
 
 # Register your models here.
+admin.site.register (Viajes)
+admin.site.register (Recreadores)
+admin.site.register (Clientes)
+admin.site.register (Proveedores)
+admin.site.register (Documentacion)
